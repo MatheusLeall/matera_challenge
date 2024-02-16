390776d0d32086acad7fb0f17a783ac99947c38d
MatheusLeall <matheuscardoso.pro@gmail.com>
2024-02-15 22:45:32 -0300

    feat: create loan and payment models

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+
+class Loan(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nominal_value = models.DecimalField(max_digits=10, decimal_places=2)
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    ip_address = models.GenericIPAddressField()
+    request_date = models.DateField(auto_now_add=True)
+    bank = models.CharField(max_length=255)
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.client.username} - {self.id}"
+
+
+class Payment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    loan = models.ForeignKey(Loan, on_delete=models.CASCADE)
+    payment_date = models.DateField()
+    payment_value = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.loan.client.username} - {self.id}"
