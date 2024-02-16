3138902330b38dc73442f9e121b45296a0a32395
MatheusLeall <matheuscardoso.pro@gmail.com>
2024-02-16 11:34:10 -0300

    feat: add iof rate in loan model

--- loans/models.py
+++ loans/models.py
@@ -14,15 +14,20 @@ class Loan(models.Model):
     request_date = models.DateField(auto_now_add=True)
     bank = models.CharField(max_length=255)
     client = models.ForeignKey(User, on_delete=models.CASCADE)
+    iof_rate = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
 
     def calculate_remaining_balance(self):
         payments = Payment.objects.filter(loan=self)
         total_payed = sum(p.payment_value for p in payments)
         days_passed = (date.today() - self.request_date).days
-        acummulated_rates = (
+        accumulated_rates = (
             (self.interest_rate / 30) * days_passed * (self.nominal_value - total_payed)
         )
-        return self.nominal_value + acummulated_rates - total_payed
+        iof_cost = self.iof_rate * self.nominal_value
+        remaining_balance = (
+            self.nominal_value + accumulated_rates + iof_cost - total_payed
+        )
+        return remaining_balance
 
     def __str__(self):
         return f"{self.client.username} - {self.id}"
