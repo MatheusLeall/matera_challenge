0cdf7ce03af3cc931478be330537607eca1633cf
MatheusLeall <matheuscardoso.pro@gmail.com>
2024-02-15 23:05:25 -0300

    feat(admin): register new models in admin page

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from loans.models import Payment
+from loans.models import Loan
 
-# Register your models here.
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ("id", "nominal_value", "interest_rate", "ip_address", "request_date", "bank", "client")
+    search_fields = ("client__username", "id")
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "loan", "payment_date", "payment_value")
+    search_fields = ("loan__client__username", "id")
